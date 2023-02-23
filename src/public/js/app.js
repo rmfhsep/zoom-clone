1f0919ae0667a41c2e5a389a26a500eee16597fb
rmfhsep <rmfhsep@gmail.com>
2023-02-23 22:37:34 +0900

    채팅 룸 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,15 +2,54 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName = "";
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("message", input.value, roomName, () => {
+    addMessage(`Yout: ${value}`);
+  });
+}
+
+function showRoom(msg) {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
+}
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
 
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("server is done!!");
-  });
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
